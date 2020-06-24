41357508be6814e2c51adb76afbfb4791bd77337
FontesJ <61214258+FontesJ@users.noreply.github.com>
2020-06-23 23:50:09 -0300

    First
    Funcional com JOptionPane

---
+++ HSM-Projeto1/src/MediaSemestral.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class MediaSemestral {
+
+    public static void main(String[] args){
+
+        float prova[] = new float[3], projeto[] = new float[3], lista[] = new float[3],
+                contribuicao[] = new float[3];
+        double media = 0;
+
+        for(int i = 0; i<3; i++) {
+            prova[i] = Float.parseFloat(JOptionPane.showInputDialog(
+                    "Digite a nota da prova do "+ (i+1) +" bimestre: "));
+            projeto[i] = Float.parseFloat(JOptionPane.showInputDialog(
+                    "Digite a nota do projeto do "+ (i+1) +" bimestre: "));
+            lista[i] = Float.parseFloat(JOptionPane.showInputDialog(
+                    "Digite a nota da lista do "+ (i+1) +" bimestre: "));
+            contribuicao[i] = Float.parseFloat(JOptionPane.showInputDialog(
+                    "Digite a nota da contribuição do "+ (8) +" bimestre: "));
+
+            media = ((prova[i]*0.3) + (projeto[i]*0.3) + (lista[i]*0.2) +
+                    (contribuicao[i]*0.3)) + media;
+        }
+        media /= 3;
+
+        if(media >= 8){
+            JOptionPane.showMessageDialog(null,"Aluno aprovado! \n" +
+                                               "Média: " + media);
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"Aluno reprovado!\n" +
+                                               "Média mínima para aprovação: 8.0\n" +
+                                               "Média: " + media);
+        }
+
+    }
+}
